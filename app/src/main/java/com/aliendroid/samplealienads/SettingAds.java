cf8e95f3bfcbf696d1f84e8c077884c0b0ee7ca1
Star Premium <starpremium.ads@gmail.com>
2022-03-31 09:30:11 +0700

    update

---
+++ app/src/main/java/com/aliendroid/samplealienads/SettingAds.java
@@ -0,0 +1,52 @@
+package com.aliendroid.samplealienads;
+
+public class SettingAds {
+    public static String SELECT_ADS ="ADMOB";
+    public static String BACKUP_ADS = "IRON";
+
+    public static String MAIN_ADS_BANNER ="ca-app-pub-3940256099942544/2247696110";
+    public static String BACKUP_ADS_BANNER="DefaultBanner";
+
+    public static String MAIN_ADS_INTERTITIAL ="ca-app-pub-3940256099942544/1033173712";
+    public static String BACKUP_ADS_INTERTITIAL="Game_Screen";
+
+    public static String MAIN_ADS_REWARDS ="ca-app-pub-3940256099942544/5224354917";
+    public static String BACKUP_ADS_REWARDS="DefaultRewardedVideo";
+
+    public static String OPEN_ADS_ADMOB ="ca-app-pub-3940256099942544/3419835294";
+    public static String NATIVE_ADS_ADMOB ="/6499/example/native";
+    public static String INITIALIZE_SDK = "3963931";
+    public static String INITIALIZE_SDK_BACKUPADS = "107355779";
+
+    public static String HIGH_PAYING_KEYWORD1="Finance";
+    public static String HIGH_PAYING_KEYWORD2="";
+    public static String HIGH_PAYING_KEYWORD3="";
+    public static String HIGH_PAYING_KEYWORD4="";
+    public static String HIGH_PAYING_KEYWORD5="";
+
+    public static int INTERVAL = 1;
+        /*
+    APPLOVIN_BANNER = "db4d5e8718b97d78";
+    APPLOVIN_INTER = "518cd97722c60b52";
+
+    BANNER_MOPUB = "b195f8dd8ded45fe847ad89ed1d016da";
+    INTER_MOPUB = "24534e1901884e398f1253216226017e";
+
+    ADMOB_INTER = "ca-app-pub-3940256099942544/1033173712";
+    ADMOB_BANNER = "ca-app-pub-3940256099942544/6300978111";
+    ADMOB_OPENAD ="ca-app-pub-3940256099942544/3419835294"
+
+    IRON_BANNER="DefaultBanner";
+    IRON_INTERTITIAL="Game_Screen";
+    IRON_REWARD="DefaultRewardedVideo
+    IRON_APPID="107355779";
+
+    STARTAPPID="123456789";
+
+    FAN_BANNER = " MG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID"
+     */
+
+
+
+
+}
